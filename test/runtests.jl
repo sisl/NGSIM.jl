d3e95e7d2bad0696fa67becac9f3f7eff8e69659
Tim Wheeler <tawheeler@users.noreply.github.com>
2017-10-16 08:18:32 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,4 @@ let
     roadway_80 = open(io->read_dxf(io, Roadway, dist_threshold_lane_connect=2.0), Pkg.dir("NGSIM", "data", "ngsim_80.dxf"), "r")
 end
 
-nbinclude(joinpath(dirname(@__FILE__), "..", "jnotebooks", "Demo.ipynb"))
+# nbinclude(joinpath(dirname(@__FILE__), "..", "jnotebooks", "Demo.ipynb"))
