91f323185627f3c24e2dedcb7e9df5ab7f59eba4
MaximeBouton <maxim.bouton@gmail.com>
2019-03-20 16:25:40 -0700

    restore a few tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
 using Test
+using AutoViz
 using NGSIM
 using NBInclude
 
-# nbinclude(joinpath(dirname(@__FILE__), "..", "jnotebooks", "Demo.ipynb"))
+@nbinclude(joinpath(dirname(pathof(NGSIM)), "..", "jnotebooks", "Demo.ipynb"))
+
+# the following test requires downloading the dataset
+# td1 = load_trajdata(1)
+# scene = get!(Scene(500), td1, 1000)
+# render(scene, ROADWAY_101)
